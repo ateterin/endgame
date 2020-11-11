03629e7da821ffaae6d3599f4ce8999364d7d0cd
helnwein <wizardsdoexist@protonmail.com>
2020-11-11 06:52:26 +0200

    .

---
+++ src/defs.h
@@ -0,0 +1,13 @@
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define MAX(a,b) (((a)>(b))?(a):(b))
+
+#define SCREEN_WIDTH   700 
+#define SCREEN_HEIGHT  800
+
+#define PLAYER_SPEED          4
+#define PLAYER_BULLET_SPEED   20
+
+#define MAX_KEYBOARD_KEYS 350
+
+#define SIDE_PLAYER 0
+#define SIDE_ALIEN  1
